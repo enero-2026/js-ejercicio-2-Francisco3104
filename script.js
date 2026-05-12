38a40d78c7f5319faa5a7eab673d24009c8b01c4
Francisco3104 <emiliano.s3104@gmail.com>
2026-05-11 22:44:29 -0600

    Funcionalidad completa

---
+++ script.js
@@ -0,0 +1,39 @@
+var busqueda = document.getElementById("busqueda");
+var btn = document.getElementById("buscar");
+var cat = document.getElementById("categorias");
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    mostrarResultados();
+});
+
+async function obtenerProductos() {
+    try {
+        const respuesta = await fetch("https://fakestoreapi.com/products");
+        const productos = await respuesta.json();
+        return productos;
+
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+async function mostrarResultados(){
+    var productos = await obtenerProductos();
+    busqueda.innerHTML = "";
+    document.getElementById("resultados").innerHTML = "";
+    var filtrados = productos.filter(producto => {
+        var coincideNombre =
+            producto.title.toLowerCase().includes(busqueda.value.toLowerCase());
+        var coincideCategoria =
+            cat.value === "" || producto.category === cat.value;
+        return coincideNombre && coincideCategoria;
+    });
+
+    filtrados.forEach(producto => {
+        var li = document.createElement("li");
+        var t = document.createElement("p");
+        t.textContent = producto.title;
+        li.appendChild(t);
+        document.getElementById("resultados").appendChild(li);
+    });
+}
